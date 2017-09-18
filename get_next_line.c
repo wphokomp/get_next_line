9eaa87ec5cc1bab7b27b54caefc26be2d7e1e600
William PHOKOMPE <wphokomp@robotic-lab-3.wethinkcode.co.za>
2017-09-18 10:42:42 +0200

    Working gnl with different BUFF_SIZEs and different line lengths(number of characters)

--- get_next_line.c
+++ get_next_line.c
@@ -5,77 +5,50 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wphokomp <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/06/17 09:03:41 by wphokomp          #+#    #+#             */
-/*   Updated: 2017/07/05 13:06:31 by wphokomp         ###   ########.fr       */
+/*   Created: 2017/09/13 10:48:54 by wphokomp          #+#    #+#             */
+/*   Updated: 2017/09/18 10:29:09 by wphokomp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "../../Filler_2/libft/libft.h"
 
-static int	countlines(char **line)
+static int	chk_rd(char **str, char *buff, char **line, int indx)
 {
-	int i;
+	char	*tmp;
 
-	i = 0;
-	while (line[i] != 0)
-		i++;
-	return (i);
-}
-
-static int	displayline(char **line)
-{
-	static int	index;
-	int			total;
-	static char	**lines;
-
-	lines = ft_strsplit(line[0], '\n');
-	total = countlines(lines);
-	if (index < total)
+	buff[indx] = '\0';
+	if ((tmp = ft_strchr(buff, '\n')))
 	{
-		ft_putstr(lines[index]);
-		index++;
+		*tmp = '\0';
+		*line = ft_strjoin(*str, buff);
+		tmp = NULL;
+		free(buff);
+		buff = 0;
+		return (1);
 	}
-	else
-		return (0);
-	return (1);
-}
-
-static int	addlines(const int fd, char **line)
-{
-	int			rd;
-	char	 buff[BUFF_SIZE + 1];
-
-	line[0] = ft_strnew(1);
-	ft_bzero((void*)line[0], (1));
-	while ((rd = read(fd, buff, BUFF_SIZE)))
-	{
-		buff[rd] = 0;
-		line[0] = ft_strjoin(line[0], buff);
-	}
-	return (rd);
+	return (0);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	int			feed;
-	static int	isread;
-	char		**ln;
+	char	*str;
+	char	*tmp;
+	char	*buff;
+	int		indx;
 
-	ft_strdel(line);
-	line = (char**)malloc(sizeof(line) * (2));
-	if (line == NULL)
-		return (-1);
-	line[1] = 0;
-	if (isread != 1)
-		if ((feed = addlines(fd, line)) == -1)
-			return (-1);
-	if (isread != 1)
+	str = ft_strnew(BUFF_SIZE);
+	buff = ft_strnew(BUFF_SIZE);
+	while ((indx = read(fd, buff, BUFF_SIZE)))
 	{
-		ln = line;
-		isread = 1;
+		if (chk_rd(&str, buff, line, indx))
+		{
+			free(str);
+			str = 0;
+			return (1);
+		}
+		tmp = str;
+		str = ft_strjoin(str, buff);
+		free(tmp);
 	}
-	if ((feed = displayline(ln)) == -1)
-		return (-1);
-	return (feed);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,18 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wphokomp <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/06/14 14:30:55 by wphokomp          #+#    #+#             */
-/*   Updated: 2017/07/05 13:05:11 by wphokomp         ###   ########.fr       */
+/*   Created: 2017/09/13 10:47:41 by wphokomp          #+#    #+#             */
+/*   Updated: 2017/09/13 10:48:48 by wphokomp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
 # include <stdlib.h>
-# include <unistd.h>
-
-# define BUFF_SIZE 42
+# include "libft/libft.h"
+# define BUFF_SIZE 10
 
 int		get_next_line(const int fd, char **line);
 
